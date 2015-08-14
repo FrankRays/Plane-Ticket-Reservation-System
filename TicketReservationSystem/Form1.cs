6dc52605565cd3b76ddea049cf0d6dbce318e039
Raydivine <raydivine00@gmail.com>
2015-08-14 17:10:52 +0800

    Customer can login , doing Reserve Menu

--- TicketReservationSystem/Form1.cs
+++ TicketReservationSystem/Form1.cs
@@ -7,36 +7,100 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace TicketReservationSystem
 {
-    public partial class Form1 : Form
+    public partial class CustomerLogin : Form
     {
-        public Form1()
+        OleDbConnection AirAsianDataBase;
+        OleDbCommand oledbCmd = new OleDbCommand();
+        string connectionString =
+         //   @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Jet\Desktop\Plane-Ticket-Reservation-System\AirAsianDataBase.mdb";
+              @"Provider = Microsoft.Jet.OLEDB.4.0; Data Source = C:\Users\Ray\Desktop\Plane-Ticket-Reservation-System-master\AirAsianDataBase.mdb";
+        public CustomerLogin()
         {
             InitializeComponent();
+            AirAsianDataBase = new OleDbConnection(connectionString);
         }
 
         private void btnRegister_Click(object sender, EventArgs e)
         {
-            Customer newCust;
-            string name = txtName.Text, 
-                   custType = cboxCustType.SelectedItem.ToString();
-           
-            if (custType == "Regular"){   
-             //   newCust = new Regular(name);
-                msgCustDetails.Text = displayCustMsg(name, custType);
-            }
-            else if (custType == "Member"){
-            //    newCust = new Member(name);
-                msgCustDetails.Text = displayCustMsg(name, custType);
-            }
+            AirAsianDataBase.Open();
+            try
+            {
+                string name = txtName.Text;
+                string email = txtEmail.Text;
+                string contactnumber = txtHP.Text;
+                string gender;
 
-            cboxCustType.Enabled = false;
-            txtName.Enabled = false;
+                if (MaleRadioButton.Checked == true)
+                    gender = "Male";
+                else
+                    gender = "Female";
+
+                int birthdayDate = Convert.ToInt16(comboBoxDate.SelectedItem.ToString());
+                int birthdayMonth = Convert.ToInt16(comboBoxMonth.SelectedItem.ToString());
+                int birthdayYear = Convert.ToInt16(comboBoxYear.SelectedItem.ToString());
+                string membership = cboxCustType.SelectedItem.ToString();
+
+                string insertstring = "Insert into Customer ([FullName], [Gender], [BirthdayDate], [BirthdayMonth], [BirthdayYear], [Contact], [Email], [MemberShip]) values (@name,@gender,@birthdayDate,@birthdayMoth,@birthdayYear,@contactnumber, @email, @membership)";
+
+                oledbCmd.Connection = AirAsianDataBase;
+                oledbCmd.CommandText = insertstring;
+                oledbCmd.Parameters.AddWithValue("@name", name);
+                oledbCmd.Parameters.AddWithValue("@gender", gender);
+                oledbCmd.Parameters.AddWithValue("@birthdayDate", birthdayDate);
+                oledbCmd.Parameters.AddWithValue("@birthdayMonth", birthdayMonth);
+                oledbCmd.Parameters.AddWithValue("@birthdayYear", birthdayYear);
+                oledbCmd.Parameters.AddWithValue("@contactnumber", contactnumber);
+                oledbCmd.Parameters.AddWithValue("@email", email);
+                oledbCmd.Parameters.AddWithValue("@membership", membership);
+
+                int temp = oledbCmd.ExecuteNonQuery();
+
+                if (temp > 0)
+                    MessageBox.Show("Registered successfully!");
+                else
+                    MessageBox.Show("Error occured! Unable to register!");
+            }
+            catch (Exception ex)
+            { MessageBox.Show(ex.Message); }
+       
+
+            AirAsianDataBase.Close();                // close the connection to the database
         }
 
-        public string displayCustMsg(string name, string custType)
-        { return "Your Name : " + name + "\nYour Type : " + custType; }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCustLogin_Click(object sender, EventArgs e)
+        {
+            AirAsianDataBase.Close();
+         
+          
+            string strSelect = "Select * from Customer Where [FullName] = @custName";
+            OleDbDataAdapter da = new OleDbDataAdapter(strSelect, AirAsianDataBase);
+            da.SelectCommand.Parameters.AddWithValue("@custName", txtCustName.Text);
+            OleDbCommandBuilder cBuilder = new OleDbCommandBuilder(da);
+            
+            DataTable dataTable = new DataTable();
+                 
+            da.Fill(dataTable);
+            if (dataTable.Rows.Count > 0)
+            {
+                MessageBox.Show("Welcome");
+                this.Hide();
+                 
+                ReserveMenu r = new ReserveMenu(txtCustName.Text);
+                r.ShowDialog();
+            }
+            else MessageBox.Show("Login Fail, please try again!");
+   
+            AirAsianDataBase.Close();
+        }
     }
 }
---
+++ TicketReservationSystem/ReserveMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicketReservationSystem
+{
+    public partial class ReserveMenu : Form
+    {
+        string custName;
+        public ReserveMenu(string name)
+        {
+            custName = name;
+            InitializeComponent();
+        }
+
+        private void ReserveMenu_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome " + this.custName + " !");
+        }
+    }
+}
